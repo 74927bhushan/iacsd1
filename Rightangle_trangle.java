f2d18057ee2fda51ff5c24ec2d2058b1131e207c
Bhushan Choudhary <127931954+74927bhushan@users.noreply.github.com>
2023-12-22 00:05:57 +0530

    Add files via upload

---
+++ Rightangle_trangle.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class Rightangle_trangle {
+
+	public static void main(String[]args) {
+	int rows=11;
+	
+	for(int i=1;i<=rows;i++) {
+		
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+			
+			
+		}
+		System.out.println();
+		
+	}
+}
+}
